92307b5cf49d266e1e63cc7ed300f3e89a00ef4a
Marco 9726 <marcosarra9@gmail.com>
2023-03-30 18:53:09 +0200

    create advancedsearch

---
+++ src/pages/AdvancedSearch.vue
@@ -0,0 +1,27 @@
+<script>
+import axios from 'axios';
+import {store} from '../store.js';
+import Searchbar from '../components/Searchbar.vue';
+export default {
+	name: 'SingleApartment',
+	components: {
+		Searchbar
+	},
+	data(){
+		return{
+			store
+		}
+	}
+	
+}
+</script>
+<template>
+	<main>
+		<div class="container">
+			<Searchbar> </Searchbar>
+		</div>	
+	</main>
+</template>
+<style lang="scss">
+	
+</style>
