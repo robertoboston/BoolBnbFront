6bc5d6015d2c9a5957c7b7c76e33d96ada3d883f
Marco 9726 <marcosarra9@gmail.com>
2023-04-07 11:19:38 +0200

    fix register language in header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -37,7 +37,7 @@ export default {
                     <a href="http://127.0.0.1:8000/login" class="fw-bolder menu_items">Login</a>
 				</div>
 				<div>
-					<a href="http://127.0.0.1:8000/register" class="fw-bolder menu_items">Register</a>
+					<a href="http://127.0.0.1:8000/register" class="fw-bolder menu_items">Registrati</a>
 				</div>
                     
                     
